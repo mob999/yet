bcd232f08efe488c85b6f36fa43eec01eacdaa89
mob999 <1790758006@qq.com>
2026-01-29 15:31:32 +0800

    chore: Update default API base URL from localhost to a public IP address.

--- yet_flutter/lib/src/services/config_service.dart
+++ yet_flutter/lib/src/services/config_service.dart
@@ -2,7 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ConfigService {
   static const String _baseUrlKey = 'api_base_url';
-  static const String defaultBaseUrl = 'http://127.0.0.1:8000';
+  static const String defaultBaseUrl = 'http://39.97.229.20:8000';
 
   final SharedPreferences _prefs;
 
